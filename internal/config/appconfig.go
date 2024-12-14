78c493f4288a697030a9f2bf9dd4b7919049a3bd
Artem Misko <nospam@misko.su>
2024-12-14 21:58:34 +0300

    Add check for non exist file with passwords. If file <servicename>.json not found then skip load secrets

--- internal/config/appconfig.go
+++ internal/config/appconfig.go
@@ -15,16 +15,19 @@ type (
 )
 
 func ReadPwd() error {
-	pwdFile, err := os.ReadFile(fmt.Sprint("./", Cfg.BaseApp.Name, ".json"))
-	if err != nil {
-		return err
-	}
+	fname := fmt.Sprint("./", Cfg.BaseApp.Name, ".json")
+	if _, err := os.Stat(fname); err == nil {
+		pwdFile, err := os.ReadFile(fname)
+		if err != nil {
+			return err
+		}
 
-	if err = json.Unmarshal(pwdFile, &pwd); err != nil {
-		return err
-	}
+		if err = json.Unmarshal(pwdFile, &pwd); err != nil {
+			return err
+		}
 
-	Cfg.App.AuthSrvPassword = pwd["app.authSrvPassword"]
+		Cfg.App.AuthSrvPassword = pwd["app.authSrvPassword"]
+	}
 
 	return nil
 }
